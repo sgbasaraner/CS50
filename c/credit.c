4f57ac7c0b2cd1652f4ac60754ded01f7c3ba61d
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-08-27 15:20:06 +0300

    improve credit.c

--- c/credit.c
+++ c/credit.c
@@ -6,12 +6,13 @@ int last_digit(long long n);
 int first_sum(long long n);
 int second_last_digit(long long n);
 int second_sum(long long n);
+long long clean_last_two_digits(long long n);
 
 int main(void){
-    printf("Enter card number: ");
+    printf("Number: ");
     long long n = get_long_long();
     if (last_digit(first_sum(n) + second_sum(n)) == 0){
-        char str[256];
+        char str[length(n) + 1];
         sprintf(str, "%lld", n);
         if ((str[0] == '3') && (str[1] == '4' || str[1] == '7')){
             printf("AMEX\n");
@@ -19,11 +20,9 @@ int main(void){
             printf("VISA\n");
         } else if ((str[0] == '5') && (str[1] == '1' || str[1] == '2' || str[1] == '3' || str[1] == '4' || str[1] == '5')){
             printf("MASTERCARD\n");
-        } else {
-            printf("LEGIT\n");
         }
     } else {
-        printf("the result is %i, it should've been 0: NOT LEGIT\n",last_digit(first_sum(n) + second_sum(n)));
+        printf("INVALID\n");
     }
 }
 
@@ -38,33 +37,35 @@ int length(long long n){
 
 int first_sum(long long n){
     int count = 0;
-    long long modified_n = n;
-    while (modified_n > 0){
-        int digit_multiplied = second_last_digit(modified_n) * 2;
+    while (n > 0){
+        int digit_multiplied = second_last_digit(n) * 2;
         int digit_multiplied_digits = digit_multiplied;
         if (digit_multiplied > 9){
             digit_multiplied_digits = second_last_digit(digit_multiplied) + last_digit(digit_multiplied);
         }
         count += digit_multiplied_digits;
-        modified_n = modified_n - (second_last_digit(modified_n) * 10) - last_digit(modified_n);
-        modified_n /= 100;
+        n = clean_last_two_digits(n)
     }
     return count;
 }
 
 int second_sum(long long n){
     int count = 0;
-    long long modified_n = n;
-    while (modified_n > 0){
-        count += last_digit(modified_n);
-        modified_n = modified_n - (second_last_digit(modified_n) * 10) - last_digit(modified_n);
-        modified_n /= 100;
+    while (n > 0){
+        count += last_digit(n);
+        n = clean_last_two_digits(n)
     }
     return count;
 }
 
 int last_digit(long long n){
     return n % 10;
+}
+
+long long clean_last_two_digits(long long n){
+    n = n - (second_last_digit(n) * 10) - last_digit(n);
+    n /= 100;
+    return n
 }
 
 int second_last_digit(long long n){
